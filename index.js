22ff209d638cf6d72dc11b2ebe9677391f7a2586
Shaurya Rathi <79589166+iron-coder12@users.noreply.github.com>
2021-06-10 08:59:04 +0530

    token

--- index.js
+++
@@ -1,9 +0,0 @@
-const { token, prefix } = require('./config.json');
-const aoi = require('aoi.js');
-const bot = new aoi.Bot({
-token: token',
-prefix: prefix, 
-fetchInvites: true,
-mobile: false
-});
-bot.onMessage();
