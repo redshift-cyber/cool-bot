3f0acb83216dcbd1781638517478cf23f8abbc8e
vunsh <vunshamehta@gmail.com>
2021-06-12 13:48:46 -0700

    history fixed, some hyper links have been implemented

--- commands/invite.js
+++ commands/invite.js
@@ -4,9 +4,9 @@ module.exports = {
     description: "replies with a message", //this part is less important and is just for details
     async execute (client, message, args){ // This is like a message event and will pass in some variables
         const inviteembed = new DiscordJS.MessageEmbed()
-        .setDescription("Here is my invite link : https://discord.com/api/oauth2/authorize?client_id=847677148499542016&permissions=8&scope=bot"
-        )
+        .setTitle('Click here for the bot invite link!')
         .setColor('BLUE')
+        .setURL('https://discord.com/api/oauth2/authorize?client_id=847677148499542016&permissions=8&scope=bot')
       message.channel.send(inviteembed)
 }
 }
