887c6b0a1a4c3f32000b77f73b19b8f3e738426f
Shaurya Rathi <79589166+iron-coder12@users.noreply.github.com>
2021-07-12 10:47:24 +0530

    Add slogan command!

---
+++ commands/information/slogan.js
@@ -0,0 +1,11 @@
+const DiscordJS = require('discord.js')
+module.exports = {
+
+	name: "slogan",
+	description: "The bot's slogan",
+	async execute (client, message, args){
+
+message.channel.send("My slogan is: Don't be a fool be cool!")
+
+}
+}
