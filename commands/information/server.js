264808292a5553b4a720987d61d2cd118d4e2fd6
Shaurya Rathi <79589166+iron-coder12@users.noreply.github.com>
2021-07-14 10:02:17 +0530

    Delete server.js

--- commands/information/server.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'server',
-	description: 'Display info about this server.',
-	execute(client, message, args) {
-		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-	},
-};
