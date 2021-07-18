229524875076289468ced24e1b425ecf664d7c62
Shaurya Rathi <79589166+iron-coder12@users.noreply.github.com>
2021-07-18 16:08:46 +0530

    Delete commands directory

--- commands/information/membercount.js
+++
@@ -1,9 +0,0 @@
-const DiscordJS = require('discord.js')
-module.exports = {
-    name: 'membercount', //Whatever u put here will be what the command is called by (example, >hi)
-    description: "provides server info", //this part is less important and is just for details
-    async execute (client, message, args){ // This is like a message event and will pass in some variables
-
-message.channel.send(`Total Members: ${message.guild.memberCount}`)
-
-    }}
