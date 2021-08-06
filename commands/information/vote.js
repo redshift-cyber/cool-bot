1cf2a67f00a573de480765189812cad767fe7a2c
Shaurya Rathi <79589166+iron-coder12@users.noreply.github.com>
2021-08-06 11:57:53 +0530

    Delete commands directory

--- commands/information/vote.js
+++
@@ -1,8 +0,0 @@
-const DiscordJS = require('discord.js')
-module.exports = {
-    name: 'vote', //Whatever u put here will be what the command is called by (example, >hi)
-    description: "returns a link to vote for the bot", //this part is less important and is just for details
-    async execute (client, message, args){ // This is like a message event and will pass in some variables
-      message.channel.send("here is the link to vote us on top.gg : url name")
-}
-}
